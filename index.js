5e977350b5813efaa4a3a337bdb0c59cd154d7a0
Vignesh Narayanan <vignesh51186@gmail.com>
2025-06-26 15:07:16 +1000

    Add Buildkite pipeline config and fix dependencies

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-// Change the default port from 3000 to 3100
 const PORT = process.env.PORT || 3100;
 
 app.get('/', (req, res) => {
